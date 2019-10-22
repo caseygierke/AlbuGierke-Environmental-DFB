826fc4f6cdb326e78a27b2f19ac4a34b3457216d
Casey <caseygierke@gmail.com>
2019-10-21 22:19:31 -0600

    Add other pages to link to in ~/pages/views and ~/pages/urls.

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, ContactPageView, ServicesPageView, ToolsPageView
 
 urlpatterns = [
 	path('', HomePageView.as_view(), name='home'),
+	path('about/', AboutPageView.as_view(), name='about'),
+	path('contact/', ContactPageView.as_view(), name='contact'),
+	path('services/', ServicesPageView.as_view(), name='services'),
+	path('tools/', ToolsPageView.as_view(), name='tools'),
 ]
