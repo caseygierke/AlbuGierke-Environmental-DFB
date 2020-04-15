e6e6934e1c307734626a3268b3d9d91152953a29
Casey Gierke <caseygierke@gmail.com>
2020-04-15 16:54:03 +0000

    Current state of website as of 4/15/2020.

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.albugierke-environmental.com']
 
 
 # Application definition
